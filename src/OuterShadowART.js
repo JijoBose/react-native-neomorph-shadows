228afbe6a6eee01a41b86003bd5003794c9012c5
Jijo Bose <bosejijo@gmail.com>
2024-03-09 01:11:52 +0530

    migrated to react-native-svg

--- src/OuterShadowART.js
+++ src/OuterShadowART.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ShadowARTType } from './types';
 import { StyleSheet } from 'react-native';
-import { Surface, Shape, Group } from '@react-native-community/art';
+import { Svg, Path, G } from 'react-native-svg';
 import { getPathWithRadius, transformShadowPropsForAndroid } from './helpers';
 
 export default class OuterShadowART extends React.PureComponent {
@@ -29,17 +29,17 @@ export default class OuterShadowART extends React.PureComponent {
     const absOffsetY = Math.abs(shadowOffset.y);
 
     return (
-      <Surface
+      <Svg
         height={height + shadowRadius * 2 + absOffsetY * 2}
         width={width + shadowRadius * 2 + absOffsetX * 2}
         style={[
           styles.surface,
           { top: -shadowRadius - absOffsetY, left: -shadowRadius - absOffsetX },
         ]}>
-        <Group x={shadowRadius + absOffsetX} y={shadowRadius + absOffsetY}>
-          <Shape d={path} fill={backgroundColor} {...shadowProps} />
-        </Group>
-      </Surface>
+        <G x={shadowRadius + absOffsetX} y={shadowRadius + absOffsetY}>
+          <Path d={path} fill={backgroundColor} {...shadowProps} />
+        </G>
+      </Svg>
     );
   }
 }
